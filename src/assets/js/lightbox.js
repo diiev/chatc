5a1b5f4df07d501bbf582bfcab71f09c2ee60b2e
diiev <ramzans096@gmail.com>
2023-05-14 21:58:50 +0300

    ADD BTN LIGHTBOX

--- src/assets/js/lightbox.js
+++ src/assets/js/lightbox.js
@@ -1,37 +1,83 @@
 
 function lightbox () {
-    const el = document.querySelectorAll('#gallery1');
-    el.forEach(item => {
-       item.addEventListener('click', (e)=> {
-            e.preventDefault();
-            const modal = document.createElement('div');
-            modal.style.cssText = `
-            position:fixed;
-            height: 100%;
-            width:100%;
-            background-color: rgba(0, 0, 0);
-            left: 0;
-            top:0;
-            z-index:1000;
-            overflow:hidden;
-            transition: all .6s;
-            `;
-        const modalContainer = document.createElement('img');
-         modal.append(modalContainer);
-         modalContainer.setAttribute('src', item.getAttribute('src'));
-         modalContainer.style.cssText = `
+    const el = document.querySelectorAll('[data-lightbox]');
+    const modal = document.createElement('div');
+    const img = document.createElement('img');
+    const modalBtns = document.createElement('div');
+    const close = document.createElement('div');
+    const fullscreen = document.createElement('div');
+    close.innerHTML = '<svg width="20px" height="20px" viewBox="0 0 24 24"><path class="fslightbox-svg-path" d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z"></path></svg>'; 
+    close.setAttribute('data-close', '');
+    close.style.cssText = `cursor:pointer`;
+    fullscreen.style.cssText = `cursor:pointer`;
+    fullscreen.innerHTML = '<svg width="20px" height="20px" viewBox="0 0 18 18"><path class="fslightbox-svg-path" d="M4.5 11H3v4h4v-1.5H4.5V11zM3 7h1.5V4.5H7V3H3v4zm10.5 6.5H11V15h4v-4h-1.5v2.5zM11 3v1.5h2.5V7H15V3h-4z"></path></svg>';
+
+    modalBtns.style.cssText = `
+    position: absolute;
+     top: 0px;
+     right:0px;
+     background: rgba(3,3,3,0.45);
+     display:flex;
+     align-items: center;
+     justify-content: space-between;
+    padding: 10px;
+    gap:20px;
+    `;
+    modal.style.cssText = `
+    position:fixed;
+    height: 100%;
+    width:100%;
+    background-color: rgba(0, 0, 0, 0.85);
+    left: 0;
+    top:0;
+    z-index:1000;
+    overflow:hidden;
+    `;   
+    img.style.cssText = `
          position:absolute;
-         height: 70%;
-         width: 70%;
+         height: 60%;
+         width: 60%;
          left: 50%;
          top:50%;
          transform:translateX(-50%) translateY(-50%);
-         object-fit: cover;
+         object-fit: contain;
          transition: all .6s;
-
          `;
-            document.body.append(modal);
+    el.forEach(item => {
+       item.addEventListener('click', (e)=> {
+            e.preventDefault();
+        document.body.append(modal);
+        document.body.style.overflow = 'hidden';
+         modal.append(img);
+         modal.append(modalBtns);
+         modalBtns.append(fullscreen);
+         modalBtns.append(close);
+        
+         img.setAttribute('src', item.getAttribute('src'));  
        });
+    });  
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal(modal);
+        }
+    }); 
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape') {
+            closeModal(modal);
+        }
+    });
+    function closeModal(modal) { 
+        modal.remove();
+        document.body.style.overflow = '';
+    
+    } 
+   
+    fullscreen.addEventListener('click', ()=> {
+                modal.requestFullscreen();
+    });
+    close.addEventListener('click', ()=> {
+        closeModal(modal);
     });
 }
 
--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -5,270 +5,267 @@ import 'fslightbox'
 import lightbox  from './lightbox';
 
 document.addEventListener('DOMContentLoaded', () => {    
-  lightbox();
-  function isHomepage() {
-    const pathName = window.location.pathname;
-    if (pathName == '/' || pathName == '/index.html' || pathName == '/chatc/dist/index.html') {
-      return true;
-    } else {
-      return false; 
-    }
-  }  
-
-  if (isHomepage()) { 
-     
+  try {
+    lightbox();
     gos();
- 
-  const sliderBanner = tns({
-    container: '.slider_banner',
-    items: 1,
-    nav: false,
-    controls: false
-  });
-  document.querySelector('#promo__slider-prev').addEventListener('click', () => {
-    sliderBanner.goTo('prev');
-  });
-  document.querySelector('#promo__slider-next').addEventListener('click', () => {
-    sliderBanner.goTo('next');
-  }); 
- 
-  const sliderSpec = tns({
-    container: '.slider',
-    items: 1,
-    nav: false,
-    navContainer: '.sdots',
-    controls: false,
-    responsive: {
-      500: {
-          items: 2
-      },
-     
-      900: {
-        items: 3
-      }
-    }
-  });  
-  setDotsSlider(sliderSpec, '.our_spec .container'); 
-  document.querySelector('#spec__slider-prev').addEventListener('click', () => {
-    sliderSpec.goTo('prev');
-  });
-  document.querySelector('#spec__slider-next').addEventListener('click', () => {
-    sliderSpec.goTo('next');
-  }); 
- 
 
-  const sliderQuoter = tns({
-    container: '.quoter_slider',
-    items: 1,
-    nav: false,
-    controls: false,
-    navContainer: '.sdots', 
-  }); 
-  setDotsSlider(sliderQuoter, '.quoter .container');
-  document.querySelector('#quoter__slider-prev').addEventListener('click', () => {
-    sliderQuoter.goTo('prev');
-  });
-  document.querySelector('#quoter__slider-next').addEventListener('click', () => {
-    sliderQuoter.goTo('next');
-  }); 
+ 
+    const sliderBanner = tns({
+      container: '.slider_banner',
+      items: 1,
+      nav: false,
+      controls: false
+    });
+    document.querySelector('#promo__slider-prev').addEventListener('click', () => {
+      sliderBanner.goTo('prev');
+    });
+    document.querySelector('#promo__slider-next').addEventListener('click', () => {
+      sliderBanner.goTo('next');
+    }); 
+   
+    const sliderSpec = tns({
+      container: '.slider',
+      items: 1,
+      nav: false,
+      navContainer: '.sdots',
+      controls: false,
+      responsive: {
+        500: {
+            items: 2
+        },
+       
+        900: {
+          items: 3
+        }
+      }
+    });  
+    setDotsSlider(sliderSpec, '.our_spec .container'); 
+    document.querySelector('#spec__slider-prev').addEventListener('click', () => {
+      sliderSpec.goTo('prev');
+    });
+    document.querySelector('#spec__slider-next').addEventListener('click', () => {
+      sliderSpec.goTo('next');
+    }); 
+   
+  
+    const sliderQuoter = tns({
+      container: '.quoter_slider',
+      items: 1,
+      nav: false,
+      controls: false,
+      navContainer: '.sdots', 
+    }); 
+    setDotsSlider(sliderQuoter, '.quoter .container');
+    document.querySelector('#quoter__slider-prev').addEventListener('click', () => {
+      sliderQuoter.goTo('prev');
+    });
+    document.querySelector('#quoter__slider-next').addEventListener('click', () => {
+      sliderQuoter.goTo('next');
+    }); 
+    
+   
+    const sliderLinks = tns({
+      container: '.logo_slider',
+      items: 1,
+      controls: false,
+      lazyload: true,
+      navContainer: '.sdots',
+      nav: false,
+      responsive: {
+        500: {
+            items: 2
+        },
+  
+        900: {
+          items: 3,
+  
+        }
+      }
+    });    
+    setDotsSlider(sliderLinks, '.links .container');
+    document.querySelector('#links__slider-prev').addEventListener('click', () => {
+      sliderLinks.goTo('prev'); 
+    });
+    document.querySelector('#links__slider-next').addEventListener('click', () => {  
+  
+      sliderLinks.goTo('next');  
+    });   
+  
+  
+    function setDotsSlider (slider, section) {   
+     const dots = document.querySelector(`${section} .sdots`);
+      for (let i = 0; i <  slider.getInfo().slideCount; i++) { 
+        const dot = document.createElement('li');
+        dot.classList.add('sdots__dot')
+        dots.append(dot);
+      }  
+      const dot = document.querySelectorAll(`${section} .sdots__dot`);
+      dot[0].classList.add('sdots__dot-active');
+      dot.forEach((item,i) => { 
+          item.addEventListener('click', ()=>{
+            slider.goTo(i); 
+                item.classList.add('sdots__dot-active');
+                dot.forEach(otherItem => {
+                     if (otherItem != item) {
+                      otherItem.classList.remove('sdots__dot-active')
+                     }
+                });
+              
+          });
+      });  
+       slider.events.on('indexChanged', (info)=> { 
+      dot.forEach((item,i) => { 
+        if (info.displayIndex == i + 1) {
+          item.classList.add('sdots__dot-active');
+          dot.forEach(otherItem => {
+               if (otherItem != item) {
+                otherItem.classList.remove('sdots__dot-active')
+               }
+          });
+        }
+      });
+      });
+    } 
+  }
+    catch (e) {
+      console.log(e);
+    }
+  
+    const accordion = document.querySelectorAll('.accordion_item'),
+      burger = document.querySelector('.burger'),
+      menu = document.querySelector('.menu'),
+      arrows = document.querySelectorAll('.arrow'),
+      quoter_slider = document.querySelector('.quoter'),
+      search_icon = document.querySelector('.fa-magnifying-glass');
+  
+    //accordion
+    accordion.forEach(item => {
+      item.addEventListener('click', () => {
+        item.classList.toggle('active');
+        item.nextElementSibling.classList.toggle('accordion_item_active');
+        accordion.forEach(otherItem => {
+          if (otherItem !== item) {
+            otherItem.classList.remove('active');
+            otherItem.nextElementSibling.classList.remove('accordion_item_active');
+           
+          }
+        });
+      });
+    });
+  
+     
+    // menu 
+    let isMobile = {
+      Android: function () {
+        return navigator.userAgent.match(/Android/i);
+      },
+      BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+      },
+      iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+      },
+      Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+      },
+      Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+      },
+      any: function () {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+      }
+    };
+    burger.addEventListener('click', (e) => { 
+      e.preventDefault();
+      burger.classList.toggle('active');
+      menu.classList.toggle('menu__active');
+      document.body.classList.toggle('lock');
+    });
+  
+    if (isMobile.any()) {
+      document.body.classList.add('touch');
+      arrows.forEach(arrow => {
+        arrow.addEventListener('click', (e) => {
+          e.preventDefault();
+          arrow.classList.toggle('active');
+          arrow.classList.toggle('parent');
+          arrow.nextElementSibling.classList.toggle('submenu__active');
+  
+        });
+      });
+  
+    } else {
+      document.body.classList.add('mouse');
+    }
+  
+    // Кнопка навверх
+    const btnUp = {
+      el: document.querySelector('.top_arrow'),
+      show() {
+        // удалим у кнопки класс scrollTop_hide
+        this.el.classList.remove('top_arrow_hide');
+      },
+      hide() {
+        // добавим к кнопке класс scrollTop_hide
+        this.el.classList.add('top_arrow_hide');
+      },
+      addEventListener() {
+        // при прокрутке содержимого страницы
+        window.addEventListener('scroll', () => {
+          // определяем величину прокрутки
+          const scrollY = window.scrollY || document.documentElement.scrollTop;
+          // если страница прокручена больше чем на 900px, то делаем кнопку видимой, иначе скрываем
+          scrollY > 900 ? this.show() : this.hide();
+        });
+        // при нажатии на кнопку .btn-up
+        document.querySelector('.top_arrow').onclick = () => {
+          // переместим в начало страницы
+          window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+          });
+        }
+      }
+    };
+  
+    btnUp.addEventListener();
+  
+    // svg
+    const imgElements = document.querySelectorAll('img.img-svg');
+    imgElements.forEach((img) => {
+      const imgClass = img.getAttribute('class');
+      const imgURL = img.getAttribute('src');
+  
+      fetch(imgURL)
+        .then((response) => response.text())
+        .then((data) => {
+          const parser = new DOMParser();
+          const svgElement = parser.parseFromString(data, 'image/svg+xml').querySelector('svg');
+  
+          if (typeof imgClass !== 'undefined') {
+            svgElement.setAttribute('class', `${imgClass} replaced-svg`);
+          }
+  
+          svgElement.removeAttribute('xmlns:a');
+  
+          if (!svgElement.getAttribute('viewBox') && svgElement.getAttribute('height') && svgElement.getAttribute('width')) {
+            svgElement.setAttribute('viewBox', `0 0 ${svgElement.getAttribute('height')} ${svgElement.getAttribute('width')}`);
+          }
+  
+          img.parentNode.replaceChild(svgElement, img);
+        });
+    });
+  
+  
+    // для ios
+    if (isMobile.iOS()) {
+      quoter_slider.classList.add('ios');
+      search_icon.classList.add('search_icon_ios');
+    } 
   
  
-  const sliderLinks = tns({
-    container: '.logo_slider',
-    items: 1,
-    controls: false,
-    lazyload: true,
-    navContainer: '.sdots',
-    nav: false,
-    responsive: {
-      500: {
-          items: 2
-      },
-
-      900: {
-        items: 3,
-
-      }
-    }
-  });    
-  setDotsSlider(sliderLinks, '.links .container');
-  document.querySelector('#links__slider-prev').addEventListener('click', () => {
-    sliderLinks.goTo('prev'); 
-  });
-  document.querySelector('#links__slider-next').addEventListener('click', () => {  
-
-    sliderLinks.goTo('next');  
-  });   
-
-
-  function setDotsSlider (slider, section) {   
-   const dots = document.querySelector(`${section} .sdots`);
-    for (let i = 0; i <  slider.getInfo().slideCount; i++) { 
-      const dot = document.createElement('li');
-      dot.classList.add('sdots__dot')
-      dots.append(dot);
-    }  
-    const dot = document.querySelectorAll(`${section} .sdots__dot`);
-    dot[0].classList.add('sdots__dot-active');
-    dot.forEach((item,i) => { 
-        item.addEventListener('click', ()=>{
-          slider.goTo(i); 
-              item.classList.add('sdots__dot-active');
-              dot.forEach(otherItem => {
-                   if (otherItem != item) {
-                    otherItem.classList.remove('sdots__dot-active')
-                   }
-              });
-            
-        });
-    });  
-     slider.events.on('indexChanged', (info)=> { 
-    dot.forEach((item,i) => { 
-      if (info.displayIndex == i + 1) {
-        item.classList.add('sdots__dot-active');
-        dot.forEach(otherItem => {
-             if (otherItem != item) {
-              otherItem.classList.remove('sdots__dot-active')
-             }
-        });
-      }
-    });
-    });
-  } 
-} 
-
-  const accordion = document.querySelectorAll('.accordion_item'),
-    burger = document.querySelector('.burger'),
-    menu = document.querySelector('.menu'),
-    arrows = document.querySelectorAll('.arrow'),
-    quoter_slider = document.querySelector('.quoter'),
-    search_icon = document.querySelector('.fa-magnifying-glass');
-
-  //accordion
-  accordion.forEach(item => {
-    item.addEventListener('click', () => {
-      item.classList.toggle('active');
-      item.nextElementSibling.classList.toggle('accordion_item_active');
-      accordion.forEach(otherItem => {
-        if (otherItem !== item) {
-          otherItem.classList.remove('active');
-          otherItem.nextElementSibling.classList.remove('accordion_item_active');
-         
-        }
-      });
-    });
-  });
-
    
-  // menu 
-  let isMobile = {
-    Android: function () {
-      return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function () {
-      return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function () {
-      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function () {
-      return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function () {
-      return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function () {
-      return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
-  };
-  burger.addEventListener('click', (e) => { 
-    e.preventDefault();
-    burger.classList.toggle('active');
-    menu.classList.toggle('menu__active');
-    document.body.classList.toggle('lock');
-  });
-
-  if (isMobile.any()) {
-    document.body.classList.add('touch');
-    arrows.forEach(arrow => {
-      arrow.addEventListener('click', (e) => {
-        e.preventDefault();
-        arrow.classList.toggle('active');
-        arrow.classList.toggle('parent');
-        arrow.nextElementSibling.classList.toggle('submenu__active');
-
-      });
-    });
-
-  } else {
-    document.body.classList.add('mouse');
-  }
-
-  // Кнопка навверх
-  const btnUp = {
-    el: document.querySelector('.top_arrow'),
-    show() {
-      // удалим у кнопки класс scrollTop_hide
-      this.el.classList.remove('top_arrow_hide');
-    },
-    hide() {
-      // добавим к кнопке класс scrollTop_hide
-      this.el.classList.add('top_arrow_hide');
-    },
-    addEventListener() {
-      // при прокрутке содержимого страницы
-      window.addEventListener('scroll', () => {
-        // определяем величину прокрутки
-        const scrollY = window.scrollY || document.documentElement.scrollTop;
-        // если страница прокручена больше чем на 900px, то делаем кнопку видимой, иначе скрываем
-        scrollY > 900 ? this.show() : this.hide();
-      });
-      // при нажатии на кнопку .btn-up
-      document.querySelector('.top_arrow').onclick = () => {
-        // переместим в начало страницы
-        window.scrollTo({
-          top: 0,
-          left: 0,
-          behavior: 'smooth'
-        });
-      }
-    }
-  };
-
-  btnUp.addEventListener();
-
-  // svg
-  const imgElements = document.querySelectorAll('img.img-svg');
-  imgElements.forEach((img) => {
-    const imgClass = img.getAttribute('class');
-    const imgURL = img.getAttribute('src');
-
-    fetch(imgURL)
-      .then((response) => response.text())
-      .then((data) => {
-        const parser = new DOMParser();
-        const svgElement = parser.parseFromString(data, 'image/svg+xml').querySelector('svg');
-
-        if (typeof imgClass !== 'undefined') {
-          svgElement.setAttribute('class', `${imgClass} replaced-svg`);
-        }
-
-        svgElement.removeAttribute('xmlns:a');
-
-        if (!svgElement.getAttribute('viewBox') && svgElement.getAttribute('height') && svgElement.getAttribute('width')) {
-          svgElement.setAttribute('viewBox', `0 0 ${svgElement.getAttribute('height')} ${svgElement.getAttribute('width')}`);
-        }
-
-        img.parentNode.replaceChild(svgElement, img);
-      });
-  });
-
-
-  // для ios
-  if (isMobile.iOS()) {
-    quoter_slider.classList.add('ios');
-    search_icon.classList.add('search_icon_ios');
-  } 
 
 
 });
