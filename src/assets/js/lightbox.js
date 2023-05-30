316b2a788455ba42ae16edd4fa7234c7b6beb7b8
diiev <ramzans096@gmail.com>
2023-05-30 19:54:30 +0300

    fix bug

--- src/assets/js/lightbox.js
+++ src/assets/js/lightbox.js
@@ -10,8 +10,12 @@ function lightbox() {
     const numberOfImg = document.createElement('div');
     const currentNumber = document.createElement('span');
     const allNumbers = document.createElement('span');
+    const popupBtnPrev  =  document.createElement('div');
+    const popupBtnNext  =  document.createElement('div');
     const prev = document.createElement('i');
     const next = document.createElement('i');
+    
+   
     let currentImg = 0;
     let currentScrollPos = 0;
     let x1 = null;
@@ -21,11 +25,14 @@ function lightbox() {
     popupButtons.classList.add('popup__btns');
     numberOfImg.classList.add('popup__numbers');
     img.classList.add('popup__img');
+    popupBtnNext.classList.add('popup__slideBtnsNext');
+    popupBtnPrev.classList.add('popup__slideBtnsPrev');
     prev.classList.add('slider__prev');
     next.classList.add('slider__next');
     zoom.innerHTML = `<svg  viewBox="0 0 512 512" height="18px" width="18px" fill="#fff"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM184 296c0 13.3 10.7 24 24 24s24-10.7 24-24V232h64c13.3 0 24-10.7 24-24s-10.7-24-24-24H232V120c0-13.3-10.7-24-24-24s-24 10.7-24 24v64H120c-13.3 0-24 10.7-24 24s10.7 24 24 24h64v64z"/></svg>`;
     close.innerHTML = '<svg  viewBox="0 0 384 512" height="26px" width="26px" fill="#fff"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>';
     fullscreen.innerHTML = '<svg  viewBox="0 0 448 512" height="20px" width="20px" fill="#fff"><path d="M32 32C14.3 32 0 46.3 0 64v96c0 17.7 14.3 32 32 32s32-14.3 32-32V96h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7 14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H64V352zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32h64v64c0 17.7 14.3 32 32 32s32-14.3 32-32V64c0-17.7-14.3-32-32-32H320zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v64H320c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32V352z"/></svg>';
+   
     imgs.forEach((item, i) => {
         item.addEventListener('click', (e) => {
             e.preventDefault();
@@ -34,8 +41,10 @@ function lightbox() {
             document.body.style.overflow = 'hidden';
             document.body.append(popup);
             popup.append(img);
-            popup.append(prev);
-            popup.append(next);
+            popupBtnPrev.append(prev);
+            popupBtnNext.append(next);
+            popup.append(popupBtnPrev);
+            popup.append(popupBtnNext);
             popup.append(numberOfImg);
             img.setAttribute('src', item.getAttribute('href'));
             popup.append(popupButtons);
@@ -58,18 +67,14 @@ function lightbox() {
         if (e.code === 'Escape') {
             closeModal(popup);
         }
+        console.log(e.code);
+        if (e.code === 'KeyF') {
+            setFullScreenImg();
+        }
     });
 
-    fullscreen.addEventListener('click', () => {
-        if (!document.fullscreenElement) {
-            popup.requestFullscreen();
-            fullscreen.innerHTML = '<svg  viewBox="0 0 448 512" height="20px" width="20px" fill="#fff" ><path d="M160 64c0-17.7-14.3-32-32-32s-32 14.3-32 32v64H32c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32V64zM32 320c-17.7 0-32 14.3-32 32s14.3 32 32 32H96v64c0 17.7 14.3 32 32 32s32-14.3 32-32V352c0-17.7-14.3-32-32-32H32zM352 64c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7 14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H352V64zM320 320c-17.7 0-32 14.3-32 32v96c0 17.7 14.3 32 32 32s32-14.3 32-32V384h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H320z"/></svg>';
-        }
-        else {
-            document.exitFullscreen();
-            fullscreen.innerHTML = '<svg  viewBox="0 0 448 512" height="20px" width="20px" fill="#fff"><path d="M32 32C14.3 32 0 46.3 0 64v96c0 17.7 14.3 32 32 32s32-14.3 32-32V96h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7 14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H64V352zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32h64v64c0 17.7 14.3 32 32 32s32-14.3 32-32V64c0-17.7-14.3-32-32-32H320zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v64H320c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32V352z"/></svg>';
-        }
-    });
+    fullscreen.addEventListener('click', setFullScreenImg);
+
     zoom.addEventListener('click', () => {
         img.classList.toggle('popup__img-zomming')
         if (isclick) {
@@ -80,13 +85,13 @@ function lightbox() {
             isclick = true;
             zoom.innerHTML = `<svg  viewBox="0 0 512 512" height="18px" width="18px" fill="#fff" ><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM136 184c-13.3 0-24 10.7-24 24s10.7 24 24 24H280c13.3 0 24-10.7 24-24s-10.7-24-24-24H136z"/></svg>`;
         }
-
+      
     });
     close.addEventListener('click', () => {
         closeModal(popup);
     });
-    next.addEventListener('click', getNext);
-    prev.addEventListener('click', getPrev);
+    popupBtnNext.addEventListener('click', getNext);
+    popupBtnPrev.addEventListener('click', getPrev);
 
     popup.addEventListener('touchstart', (e) => {
         x1 = e.touches[0].clientX;
@@ -134,6 +139,8 @@ function lightbox() {
 
     function closeModal(modal) {
         img.classList.remove('popup__img-zomming');
+        zoom.innerHTML = `<svg  viewBox="0 0 512 512" height="18px" width="18px" fill="#fff"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM184 296c0 13.3 10.7 24 24 24s24-10.7 24-24V232h64c13.3 0 24-10.7 24-24s-10.7-24-24-24H232V120c0-13.3-10.7-24-24-24s-24 10.7-24 24v64H120c-13.3 0-24 10.7-24 24s10.7 24 24 24h64v64z"/></svg>`;
+        isclick = false;
         document.body.style.paddingRight = '';
         document.body.style.overflow = '';
         modal.remove();
@@ -152,7 +159,16 @@ function lightbox() {
         currentNumber.innerHTML = `${currentImg + 1}`;
     }
 
-
+    function setFullScreenImg () {
+        if (!document.fullscreenElement) {
+            popup.requestFullscreen();
+            fullscreen.innerHTML = '<svg  viewBox="0 0 448 512" height="20px" width="20px" fill="#fff" ><path d="M160 64c0-17.7-14.3-32-32-32s-32 14.3-32 32v64H32c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32V64zM32 320c-17.7 0-32 14.3-32 32s14.3 32 32 32H96v64c0 17.7 14.3 32 32 32s32-14.3 32-32V352c0-17.7-14.3-32-32-32H32zM352 64c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7 14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H352V64zM320 320c-17.7 0-32 14.3-32 32v96c0 17.7 14.3 32 32 32s32-14.3 32-32V384h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H320z"/></svg>';
+        }
+        else {
+            document.exitFullscreen();
+            fullscreen.innerHTML = '<svg  viewBox="0 0 448 512" height="20px" width="20px" fill="#fff"><path d="M32 32C14.3 32 0 46.3 0 64v96c0 17.7 14.3 32 32 32s32-14.3 32-32V96h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7 14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H64V352zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32h64v64c0 17.7 14.3 32 32 32s32-14.3 32-32V64c0-17.7-14.3-32-32-32H320zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32v64H320c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32V352z"/></svg>';
+        }
+    }
 
 }
 
